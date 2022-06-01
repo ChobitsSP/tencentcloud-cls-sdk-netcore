53085fcb39ab8bb14425082457e29378e5781dda
Chao <zhangchao@hsort.com>
2022-06-01 12:04:20 +0800

    Test1

---
+++ Tencent.Cls.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using Serilog.Events;
+using System;
+using System.Diagnostics;
+using Tencent.Cls.Sdk;
+
+namespace Tencent.Cls.Test
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Test1();
+        }
+
+        static async void Test1()
+        {
+            var client = new AsyncClient();
+
+            LogEvent log = null;
+
+            var rsp = await client.EmitBatchAsync("", log);
+
+            Console.WriteLine(rsp);
+
+            Debugger.Break();
+        }
+    }
+}
